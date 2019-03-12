94001e5870b3e1d1876012c13539dd0722ede859
Sacha Epskamp <sacha.epskamp@gmail.com>
2019-03-12 20:38:46 +0100

    mu setup

---
+++ R/03_modelformation_matrixSetup_mu.R
@@ -0,0 +1,32 @@
+matrixsetup_mu <- function(
+  mu, # sigma argument
+  nNode, # Number of nodes
+  nGroup, # Number of groups
+  expmeans, # Expected covariance matrices (list).
+  labels,
+  equal = FALSE,
+  sampletable
+){
+  # Fix mu
+  mu <- fixMu(mu,nGroup,nNode,"mu" %in% equal)
+
+  # For each group, form starting values:
+  muStart <- mu
+  for (g in 1:nGroup){
+    # Current estimate:
+    meanest <-  as.matrix(expmeans[[g]])
+
+    # Covs with sample covs:
+    muStart[,g] <- 1*(mu[,g]!=0) * meanest
+  }
+
+  # Form the model matrix part:
+  list(mu,
+       mat =  "mu",
+       op =  "~1",
+       symmetrical= FALSE,
+       sampletable=sampletable,
+       rownames = labels,
+       colnames = "1",
+       start = muStart)
+}
