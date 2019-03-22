d7cd2bd13e34ca7b31a2bd3d74986b86322d18f2
Sacha Epskamp <mail@sachaepskamp.com>
2019-03-22 22:41:37 +0100

    reduced epc start

--- R/d_stepup.R
+++ R/d_stepup.R
@@ -118,7 +118,7 @@ stepup <- function(
         # # Perturb estimate a bit:
         # x@parameters$est[best] <- 0.01
         # Set estimate to EPC:
-        x@parameters$est[best] <- x@parameters$est[best] + x@parameters$epc[best]/2
+        x@parameters$est[best] <- x@parameters$est[best] + x@parameters$epc[best]/3
         
         # Update the model:
         x@extramatrices$M <- Mmatrix(x@parameters) # FIXME: Make nice function for this
@@ -143,7 +143,7 @@ stepup <- function(
         # Perturb estimate a bit:
         # x@parameters$est[best] <- 0.001
         # Set estimate to EPC:
-        x@parameters$est[best] <- x@parameters$est[best] + x@parameters$epc[best]/1
+        x@parameters$est[best] <- x@parameters$est[best] + x@parameters$epc[best]/3
         
         # Update the model:
         x@extramatrices$M <- Mmatrix(x@parameters) # FIXME: Make nice function for this
