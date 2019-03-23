74ec8fd1d2858c58a61ba133046523a836c4289d
Sacha Epskamp <mail@sachaepskamp.com>
2019-03-23 10:15:48 +0100

    Added trysolve

---
+++ R/02_algebrahelpers_trysolve.R
@@ -0,0 +1,26 @@
+trysolve <- function(x){
+  tryres <- try({
+    y <- solve(x)
+  }, silent = TRUE)
+  if (!is(tryres, "try-error")){
+    return(y)
+  }
+
+  # Try psuedoinverse:
+  tryres <- try({
+    y <- corpcor::pseudoinverse(x)
+  }, silent = TRUE)
+  if (!is(tryres, "try-error")){
+    return(y)
+  }
+  
+  # Try spectral shift:
+  tryres <- try({
+    y <- corpcor::pseudoinverse(spectralshift(x))
+  }, silent = TRUE)
+  if (!is(tryres, "try-error")){
+    return(y)
+  }
+
+  stop("Could not compute matrix inverse")
+}
