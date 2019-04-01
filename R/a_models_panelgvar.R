73239113b268d7eaed5426c03663e8ae6523ba83
Sacha Epskamp <sacha.epskamp@gmail.com>
2019-04-01 08:49:28 +0200

    panelvar and panelgvar implemented

--- R/a_models_panelgvar.R
+++ R/a_models_panelgvar.R
@@ -1,4 +1,58 @@
-panelgvar <- function(...){
-  panelvar1(...,contemporaneous = "ggm", between = "ggm")
+panelvar <- function(data,vars,...){
+  # Extract var names:
+  if (missing(vars)){
+    stop("'vars' argument may not be missing")
+  }
+  if (!is.matrix(vars)){
+    stop("'vars' must be a design matrix, with rows indicating variables and columns indicating measurements.")
+  }
+  
+  
+  I <- diag(nrow(vars))
+  O <- matrix(0,nrow(vars),nrow(vars))
+  
+  
+  if (is.null(rownames(vars))){
+    rownames(vars) <- paste0("Eta_",seq_len(nrow(vars)))
+  }
+  
+  dlvm1(data,vars,
+        lambda = I,
+        within_latent = "chol",
+        within_residual = "cov", sigma_epsilon_within = O,
+        between_latent = "chol",
+        between_residual = "cov", sigma_epsilon_between = O,
+        latents = rownames(vars),
+        ...
+        )
 }
+
+panelgvar <- function(data,vars,...){
+  # Extract var names:
+  if (missing(vars)){
+    stop("'vars' argument may not be missing")
+  }
+  if (!is.matrix(vars)){
+    stop("'vars' must be a design matrix, with rows indicating variables and columns indicating measurements.")
+  }
+  
+  
+  I <- diag(nrow(vars))
+  O <- matrix(0,nrow(vars),nrow(vars))
+  
+  if (is.null(rownames(vars))){
+    rownames(vars) <- paste0("Eta_",seq_len(nrow(vars)))
+  }
+  
+  dlvm1(data,vars,
+        lambda = I,
+        within_latent = "ggm",
+        within_residual = "cov", sigma_epsilon_within = O,
+        between_latent = "ggm",
+        between_residual = "cov", sigma_epsilon_between = O,
+        latents = rownames(vars),
+        ...
+  )
+}
+
 
