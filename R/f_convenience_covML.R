b1d7511578b6c116cfb88793d35a18f73c734554
Sacha Epskamp <ubuntu@packer-Ubuntu-18.04.2-Desktop>
2020-01-22 16:45:37 +0100

    CRAN version 0.5.0

--- R/f_convenience_covML.R
+++ R/f_convenience_covML.R
@@ -1,13 +1,13 @@
 # Maximum likelihood covariance estimate:
 covML <- function(x,...){
-  (nrow(x)-1)/(nrow(x)) * cov(x, ...)
+  (nrow(x)-1)/(nrow(x)) * stats::cov(x, ...)
 }
 
 # transform between ML and unbiased estimators:
-covMLtoUB <- function(x,n){
-  n/(n-1) * cov(x, ...)
+covMLtoUB <- function(x,n,...){
+  n/(n-1) * x
 }
 
-covUBtoML <- function(x,n){
+covUBtoML <- function(x,n,...){
   (n-1)/n * x
 }
