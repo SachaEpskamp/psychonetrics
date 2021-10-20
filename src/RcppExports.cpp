922ee73bf750c1afc6469363b0604b5330b0a34c
Sacha Epskamp <sacha.epskamp@gmail.com>
2021-10-20 10:26:49 +0200

    tsData warning

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,11 +6,6 @@
 
 using namespace Rcpp;
 
-#ifdef RCPP_USE_GLOBAL_ROSTREAM
-Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
-Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
-#endif
-
 // eig_sym_cpp
 arma::vec eig_sym_cpp(arma::mat X);
 RcppExport SEXP _psychonetrics_eig_sym_cpp(SEXP XSEXP) {
